21d9a63428a3e4b52d3a49cc0470956007137e4d
Fahersto <>
2021-07-08 20:21:26 +0200

    free memory of trampoline

--- library/src/Detour.cpp
+++ library/src/Detour.cpp
@@ -9,21 +9,20 @@ namespace hookftw
 {
 #ifdef _WIN64
 	//
-	//Hooks a function by placing jumping to a proxy function using 14 bytes:
-	//0xff, 0x25, 0x0, 0x0, 0x0, 0x0					JMP[rip + 0]
-	//0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88	absolute address of jump
-	//The proxy function is expected to return to the trampoline (return value of DetourHook::Hook()) which runs the overwritten instructions and returns back to the original code
-	//Important: the overwritten bytes are NOT relocated meaning only position independet instructions can be overwritten
+	// Hooks a function by placing jumping to a proxy function using 14 bytes:
+	// 0xff, 0x25, 0x0, 0x0, 0x0, 0x0					JMP[rip + 0]
+	// 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88	absolute address of jump
+	// the trampoline (return value of DetourHook::Hook()) runs the overwritten instructions and returns back to the original code
 	int8_t* Detour::Hook(int8_t* sourceAddress, int8_t* targetAddress)
 	{
-		//length of detour
+		// length of detour
 		const int stubJumpBackLength = 14;
 
 		Decoder decoder;
 		//TODO we currently assume that we can reach the trampole with rel32.
 		int lengthWithoutCuttingInstructionsInHalf = decoder.GetLengthOfInstructions(sourceAddress, stubJumpBackLength);
 
-		//5 bytes are required to place detour
+		// 5 bytes are required to place detour
 		assert(lengthWithoutCuttingInstructionsInHalf >= stubJumpBackLength);
 
 		//remember for unhooking
@@ -34,14 +33,12 @@ namespace hookftw
 		originalBytes_ = new int8_t[hookLength_];
 		memcpy(originalBytes_, sourceAddress, hookLength_);
 
-
-		//make page of detour address writeable
+		// make page of detour address writeable
 		DWORD pageProtection;
 		VirtualProtect(sourceAddress, hookLength_, PAGE_READWRITE, &pageProtection);
 
-		//allocate space for stub + space for overwritten bytes + jumpback
-		//TODO the calculation is technically not correct. But since we get a page worth of memory anyway it doesn't matter
-		trampoline_ = (int8_t*)VirtualAlloc(NULL, hookLength_ + stubJumpBackLength, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+		// allocate space for stub + space for overwritten bytes + jumpback
+		trampoline_ = (int8_t*)VirtualAlloc(NULL, 0x1000, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
 		if (!trampoline_)
 		{
 			printf("[Error] - Detour - Failed to allocate trampoline\n");
@@ -57,7 +54,6 @@ namespace hookftw
 		memcpy(trampoline_, relocatedBytes.data(), relocatedBytes.size());
 
 		int8_t* addressAfterRelocatedBytes = trampoline_ + relocatedBytes.size();
-
 
 		//write JMP back from trampoline to original code
 		addressAfterRelocatedBytes[0] = 0xFF;														//opcodes = JMP [rip+0]
@@ -85,8 +81,6 @@ namespace hookftw
 		return trampoline_;
 	}
 
-
-
 	void Detour::Unhook()
 	{
 		//make page writeable
@@ -101,45 +95,42 @@ namespace hookftw
 
 		//clean up allocated memory
 		delete[] originalBytes_;
-
-		//memory leak but enables unhooking inside hooked function and makes it threadsafe?
-		//delete[] trampoline;
+		delete[] trampoline_;
 	}
 
 #else
 	//
-	//Hooks a function by placing a jump (JMP 0xE9) from the original function to the proxy function
-	//The proxy function is expected to return to the trampoline (return value of DetourHook::Hook()) which runs the overwritten instructions and returns back to the original code
-	//Only for x86 since JMP (0xE9) hast 32bit target address
-	//Important: the overwritten bytes are NOT relocated meaning only position independet instructions can be overwritten
+	// Hooks a function by placing a jump (JMP 0xE9) from the original function to the proxy function
+	// The proxy function is expected to return to the trampoline (return value of DetourHook::Hook()) which runs the overwritten instructions and returns back to the original code
+	// Only for x86 since JMP (0xE9) hast 32bit target address
+	// Important: the overwritten bytes are NOT relocated meaning only position independet instructions can be overwritten
 	int8_t* Detour::Hook(int8_t* sourceAddress, int8_t* targetAddress)
 	{
-		//length of detour
+		// length of detour
 		const int stubJumpBackLength = 5;
 
 		Decoder decoder;
 
-		//TODO we currently assume that we can reach the trampole with rel32.
+		// TODO we currently assume that we can reach the trampole with rel32.
 		int lengthWithoutCuttingInstructionsInHalf = decoder.GetLengthOfInstructions(sourceAddress, stubJumpBackLength);
 
-		//5 bytes are required to place detour
+		// 5 bytes are required to place detour
 		assert(lengthWithoutCuttingInstructionsInHalf >= stubJumpBackLength);
 
-		//remember for unhooking
+		// remember for unhooking
 		this->hookLength_ = lengthWithoutCuttingInstructionsInHalf;
 		this->sourceAddress_ = sourceAddress;
 
-		//save original bytes
+		// save original bytes
 		originalBytes_ = new int8_t[hookLength_];
 		memcpy(originalBytes_, sourceAddress, hookLength_);
 
-
-		//make page of detour address writeable
+		// make page of detour address writeable
 		DWORD pageProtection;
 		VirtualProtect(sourceAddress, hookLength_, PAGE_READWRITE, &pageProtection);
 
-		//allocate trampoline
-		//TODO check fir rip relative instructions if we can reach original targets with rel32
+		// allocate trampoline
+		// TODO check for rip-relative instructions if we can reach original targets with rel32
 		trampoline_ = (int8_t*)VirtualAlloc(NULL, hookLength_ + stubJumpBackLength, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
 		if (!trampoline_)
 		{
@@ -152,54 +143,52 @@ namespace hookftw
 			printf("[Error] - Detour - Relocation of bytes replaced by hook failed\n");
 		}
 
-		//copy overwritten bytes to trampoline
+		// copy overwritten bytes to trampoline
 		memcpy(trampoline_, relocatedBytes.data(), relocatedBytes.size());
 
 		int8_t* addressAfterRelocatedBytes = trampoline_ + relocatedBytes.size();
 
-		//write JMP back from trampoline to original code
+		// write JMP back from trampoline to original code
 		addressAfterRelocatedBytes[0] = 0xE9;
 		*(uint32_t*)(addressAfterRelocatedBytes + 1) = (uint32_t)(sourceAddress + hookLength_ - addressAfterRelocatedBytes) - 5;
 
-		//write JMP from original code to hook function
+		// write JMP from original code to hook function
 		sourceAddress[0] = 0xE9;
 		*(uint32_t*)(sourceAddress + 1) = (uint32_t)(targetAddress - sourceAddress) - 5;
 
-		//NOP left over bytes
+		// NOP left over bytes
 		for (int i = stubJumpBackLength; i < hookLength_; i++)
 		{
 			sourceAddress[i] = 0x90;
 		}
 
-		//restore page protection
+		// restore page protection
 		VirtualProtect(sourceAddress, hookLength_, pageProtection, &pageProtection);
 
-		//make trampoline executable
+		// make trampoline executable
 		VirtualProtect(trampoline_, relocatedBytes.size() + 5, PAGE_EXECUTE_READWRITE, &pageProtection);
 	
-		//return the address of the trampoline so we can call it to invoke the original function
+		// return the address of the trampoline so we can call it to invoke the original function
 		return trampoline_;
 	}
 
 	//
-	//Unhooks a previously hooked function by copying back the original bytes
+	// Unhooks a previously hooked function by copying back the original bytes
 	void Detour::Unhook()
 	{
-		//make page writeable
+		// make page writeable
 		DWORD dwback;
 		VirtualProtect(sourceAddress_, hookLength_, PAGE_READWRITE, &dwback);
 
-		//copy back original bytes
+		// copy back original bytes
 		memcpy(sourceAddress_, originalBytes_, hookLength_);
 
-		//restore page protection
+		// restore page protection
 		VirtualProtect(sourceAddress_, hookLength_, dwback, &dwback);
 
-		//clean up allocated memory
+		// clean up allocated memory
 		delete[] originalBytes_;
-
-		//memory leak but enables unhooking inside hooked function and makes it threadsafe?
-		//delete[] trampoline;
+		delete[] trampoline_;
 	}
 #endif
 
