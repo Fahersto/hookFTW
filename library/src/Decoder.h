d46b064f847be4b8b3f8bf1e1104634faded9825
Fahersto <31221314+Fahersto@users.noreply.github.com>
2021-08-05 18:11:04 +0200

    moved printing of instructions to decoder class

--- library/src/Decoder.h
+++ library/src/Decoder.h
@@ -17,18 +17,19 @@ namespace hookftw
 	/**
 	 * \brief Decodes the target binary and provides utilities to work with assembly instructions
 	 *
-	 * Disassembles the target binary.
+	 * Uses the Zydis Disassembler to analyse and/or relocate assembler instructions.
 	 */
 	class Decoder
 	{
 		// we use a void pointer here since we can't forward declare the ZydisDecoder c typedef struct
-		// we do not want to include the zydis headers here since we then have to link against zydis (and not only hookftw) when using hookftw
+		// we do not want to include the zydis headers here since we then have to link against zydis (and not only hookFTW) when using hookFTW
 		static void* _zydisDecoder;
 
 		const int MAXIMUM_INSTRUCTION_LENGTH = 15;
 	public:
 		Decoder();
 	
+		void PrintInstructions(int8_t* address, size_t byteCount);
 		int GetLengthOfInstructions(int8_t* sourceAddress, int length);
 		std::vector<int8_t*> FindRelativeInstructionsOfType(int8_t* startAddress, RelativeInstruction type, int length);
 		bool CalculateRipRelativeMemoryAccessBounds(int8_t* sourceAddress, int length, int64_t* lowestAddress, int64_t* highestAddress);
